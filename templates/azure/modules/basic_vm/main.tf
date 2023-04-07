d9b6f78a1c884b117ebcd40a2a3f27577854bdf8
tomyoungs <tomfyoungs@gmail.com>
2023-04-07 23:47:00 +0100

    fixed azurevm and added deployment script

--- templates/azure/modules/basic_vm/main.tf
+++ templates/azure/modules/basic_vm/main.tf
@@ -26,28 +26,37 @@ resource "azurerm_public_ip" "vmpublicip" {
 
 //virtual_machine
 
-resource "azurerm_linux_virtual_machine" "vm" {
+resource "azurerm_virtual_machine" "vm" {
   name                  = "${var.prefixname}-vm"
   location              = var.location
   resource_group_name   = var.rgname
   network_interface_ids = [azurerm_network_interface.nic.id]
-  size                  = var.size
-  admin_username        = "adminuser"
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/azure-vm-tf-id.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
+  vm_size               = var.size
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+  
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
   }
 }
 
